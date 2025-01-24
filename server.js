b07d37f35ff8f7128a46794f511820b4391249e8
crazyrat3 <theclockman0304@gmail.com>
2025-01-23 16:08:51 -1200

    online karbar

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const db = require("./db");
 
 const bodyParser = require("body-parser");
@@ -14,6 +15,7 @@ const personRoutes = require("./routes/personRoutes");
 const menuRoutes = require("./routes/menuItemRoutes");
 app.use("/person", personRoutes);
 app.use("/menu", menuRoutes);
+
 app.listen(3000, () => {
   console.log("listening on port 3000");
 });
